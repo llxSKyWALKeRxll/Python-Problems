90dcc0849d4d34e47459b4a28630ea5fffe0533f
llxSKyWALKeRxll <19bcs2190@gmail.com>
2021-08-07 01:55:06 +0530

    .

---
+++ Tower Of Hanoi/soln.py
@@ -0,0 +1,8 @@
+def towerOfHanoi(noOfDisks, startPeg=1, endPeg=3):
+    if noOfDisks:
+        towerOfHanoi(noOfDisks-1, startPeg, 6-startPeg-endPeg)
+        print(f"Move disk {noOfDisks} from peg {startPeg} to {endPeg}")
+        towerOfHanoi(noOfDisks-1, 6-startPeg-endPeg, endPeg)
+
+if __name__=="__main__":
+    towerOfHanoi(noOfDisks=4)
