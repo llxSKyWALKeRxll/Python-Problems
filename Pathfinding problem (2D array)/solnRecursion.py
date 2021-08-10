abf1cbf834150d2f52440048598ab685dc747d63
llxSKyWALKeRxll <19bcs2190@gmail.com>
2021-08-11 03:10:38 +0530

    .

---
+++ Pathfinding problem (2D array)/solnRecursion.py
@@ -0,0 +1,22 @@
+def pathFinder(arr, startPos, endPos):
+    if startPos == endPos:
+        return [endPos]
+    x, y = startPos
+    if x+1 <= endPos[0] and arr[x+1][y]==1:
+        a = pathFinder(arr, (x+1, y), endPos)
+        if a != None:
+            return [(x, y)] + a
+    if y+1 <= endPos[1] and arr[x][y+1]==1:
+        b = pathFinder(arr, (x, y+1), endPos)
+        if b!= None:
+            return [(x,y)] + b
+
+if __name__=="__main__":
+    arr = [
+        [1,1,1,1,0],
+        [0,1,0,1,0],
+        [0,1,0,1,0],
+        [0,1,0,0,0],
+        [1,1,1,1,1]
+    ]
+    print(*pathFinder(arr, (0,0), (1,3)), sep=" ==> ")
